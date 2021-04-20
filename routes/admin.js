a94f0d16926a7b8c21a2cbbabd26ba579850d475
marcinchaluda <chaluda@interia>
2021-04-20 19:27:30 +0200

    handle admin login

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+router.all('*', (req, res, next) => {
+   if (!req.session.admin) {
+       res.redirect('/login');
+       return;
+   }
+   next();
+});
+
 router.get('/', (req, res) => {
+
     res.render('admin', { title: 'Admin' });
 });
 
